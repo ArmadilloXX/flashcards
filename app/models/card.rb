f3574cb61956073294174aa05483b8853d7aef50
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-09-22 12:00:43 +0300

    Extract methods from Card#check_translation

--- app/models/card.rb
+++ app/models/card.rb
@@ -21,17 +21,11 @@ class Card < ActiveRecord::Base
   def check_translation(user_translation)
     distance = Levenshtein.distance(full_downcase(translated_text),
                                     full_downcase(user_translation))
-
     sm_hash = SuperMemo.algorithm(interval, repeat, efactor, attempt, distance, 1)
-
     if distance <= 1
-      sm_hash.merge!({ review_date: Time.now + interval.to_i.days, attempt: 1 })
-      update(sm_hash)
-      { state: true, distance: distance }
+      successful_review_update(sm_hash, distance)
     else
-      sm_hash.merge!({ attempt: [attempt + 1, 5].min })
-      update(sm_hash)
-      { state: false, distance: distance }
+      unsuccessful_review_update(sm_hash, distance)
     end
   end
 
@@ -45,6 +39,18 @@ class Card < ActiveRecord::Base
   end
 
   protected
+
+  def successful_review_update(sm_hash, distance)
+    sm_hash.merge!({ review_date: Time.now + interval.to_i.days, attempt: 1 })
+    update(sm_hash)
+    { state: true, distance: distance }
+  end
+
+  def unsuccessful_review_update(sm_hash, distance)
+    sm_hash.merge!({ attempt: [attempt + 1, 5].min })
+    update(sm_hash)
+    { state: false, distance: distance }
+  end
 
   def set_review_date_as_now
     self.review_date = Time.now
