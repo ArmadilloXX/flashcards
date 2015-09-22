8dc5c6056bbd6d2486cbedcedbcaf4c6843eaacf
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-09-22 11:39:05 +0300

    Extract some methods in TrainerController and ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,21 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    locale = if current_user
-               current_user.locale
-             elsif params[:user_locale]
-               params[:user_locale]
-             elsif session[:locale]
-               session[:locale]
-             else
-               http_accept_language.compatible_language_from(I18n.available_locales)
-             end
+    @locale = if current_user
+                current_user.locale
+              elsif params[:user_locale]
+                params[:user_locale]
+              elsif session[:locale]
+                session[:locale]
+              else
+                http_accept_language.compatible_language_from(I18n.available_locales)
+              end
+    set_session_locale
+  end
 
-    if locale && I18n.available_locales.include?(locale.to_sym)
-      session[:locale] = I18n.locale = locale
+  def set_session_locale
+    if @locale && I18n.available_locales.include?(locale.to_sym)
+      session[:locale] = I18n.locale = @locale
     else
       session[:locale] = I18n.locale = I18n.default_locale
     end
--- app/controllers/dashboard/trainer_controller.rb
+++ app/controllers/dashboard/trainer_controller.rb
@@ -4,24 +4,16 @@ class Dashboard::TrainerController < Dashboard::BaseController
     if params[:id]
       @card = current_user.cards.find(params[:id])
     else
-      set_card
+      set_card_for_review
     end
   end
 
   def review_card
     @card = current_user.cards.find(params[:card_id])
-
     check_result = @card.check_translation(trainer_params[:user_translation])
 
     if check_result[:state]
-      if check_result[:distance] == 0
-        flash[:notice] = t(:correct_translation_notice)
-      else
-        flash[:alert] = t 'translation_from_misprint_alert',
-                          user_translation: trainer_params[:user_translation],
-                          original_text: @card.original_text,
-                          translated_text: @card.translated_text
-      end
+      prepare_flash_message(check_result[:distance])
       redirect_to trainer_path
     else
       flash[:alert] = t(:incorrect_translation_alert)
@@ -35,13 +27,32 @@ class Dashboard::TrainerController < Dashboard::BaseController
     params.permit(:user_translation)
   end
 
-  def set_card
+  def set_card_for_review
     if current_user.current_block
-      @card = current_user.current_block.cards.pending.first ||
-              current_user.current_block.cards.repeating.first
+      set_card_from_current_block
     else
-      @card = current_user.cards.pending.first ||
-              current_user.cards.repeating.first
+      set_card
+    end
+  end
+
+  def set_card_from_current_block
+    @card = current_user.current_block.cards.pending.first ||
+      current_user.current_block.cards.repeating.first
+  end
+
+  def set_card
+    @card = current_user.cards.pending.first ||
+      current_user.cards.repeating.first
+  end
+
+  def prepare_flash_message(distance)
+    if distance == 0
+      flash[:notice] = t(:correct_translation_notice)
+    else
+      flash[:alert] = t 'translation_from_misprint_alert',
+                        user_translation: trainer_params[:user_translation],
+                        original_text: @card.original_text,
+                        translated_text: @card.translated_text
     end
   end
 end
