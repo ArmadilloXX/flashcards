20478681dd4ba728e237fc1af723a9755021e9e4
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-09-28 12:37:01 +0300

    Fix code style violation in trainer_spec (iter#5)

--- spec/features/trainer_spec.rb
+++ spec/features/trainer_spec.rb
@@ -5,7 +5,7 @@ include LoginHelper
 
 describe "review cards" do
   let!(:user) { create(:user) }
-  let(:update_cards) do 
+  let(:update_cards) do
     user.cards.each do |card| 
       card.update_attribute(:review_date, Time.now - 3.days)
     end
@@ -47,7 +47,7 @@ describe "review cards" do
       fill_in "user_translation", with: "RoR"
       click_button "Проверить"
       expect(page).
-          to have_content "Вы ввели не верный перевод. Повторите попытку."
+        to have_content "Вы ввели не верный перевод. Повторите попытку."
     end
     it "shows typo message when translation has a typo" do
       fill_in "user_translation", with: "hous"
@@ -58,7 +58,7 @@ describe "review cards" do
       fill_in "user_translation", with: "hou"
       click_button "Проверить"
       expect(page).
-          to have_content "Вы ввели не верный перевод. Повторите попытку."
+        to have_content "Вы ввели не верный перевод. Повторите попытку."
     end
   end
 
@@ -68,7 +68,7 @@ describe "review cards" do
     end
 
     describe "when user has one block" do
-      let!(:block) {create(:block, user: user)}
+      let!(:block) {create(:block, user: user) }
 
       describe "and no cards" do
         it_behaves_like "no cards"
@@ -109,19 +109,17 @@ describe "review cards" do
       describe "and two cards" do
         let!(:card) { create(:card, user: user, block: block) }
         let!(:card2) { create(:card, user: user, block: block) }
-
         before do
           update_cards
           visit_trainer
         end
-
         it_behaves_like "show translation results"
       end
     end
 
     describe "when user has two blocks" do
-      let!(:block) {create(:block, user: user)}
-      let!(:block2) {create(:block, user: user)}
+      let!(:block) { create(:block, user: user) }
+      let!(:block2) { create(:block, user: user) }
 
       describe "and no cards" do
         it_behaves_like "no cards"
@@ -130,23 +128,19 @@ describe "review cards" do
       describe "and one card for each block" do
         let!(:card) { create(:card, user: user, block: block) }
         let!(:card2) { create(:card, user: user, block: block2) }
-
         before do
           update_cards
           visit_trainer
         end
-
         it_behaves_like "show translation results"
       end
 
       describe "and one card" do
         let!(:card) { create(:card, user: user, block: block) }
-
         before do
           update_cards
           visit_trainer
         end
-
         it_behaves_like "show translation results"
       end
     end
@@ -164,19 +158,19 @@ describe "review cards" do
     end
 
     describe "and two cards in each block" do
-      let!(:block2) { create(:block, user: user)}
-      let!(:card) { create(:card, user: user, block: block)}
-      let!(:card2) { create(:card, user: user, block: block)}
-      let!(:card3) { create(:card, user: user, block: block2)}
-      let!(:card4) { create(:card, user: user, block: block2)}
+      let!(:block2) { create(:block, user: user) }
+      let!(:card) { create(:card, user: user, block: block) }
+      let!(:card2) { create(:card, user: user, block: block) }
+      let!(:card3) { create(:card, user: user, block: block2) }
+      let!(:card4) { create(:card, user: user, block: block2) }
 
       it_behaves_like "get cards from current_block"
     end
 
     describe "and two cards in each block" do
-      let!(:block2) { create(:block, user: user)}
-      let!(:card) { create(:card, user: user, block: block)}
-      let!(:card2) { create(:card, user: user, block: block2)}
+      let!(:block2) { create(:block, user: user) }
+      let!(:card) { create(:card, user: user, block: block) }
+      let!(:card2) { create(:card, user: user, block: block2) }
 
       it_behaves_like "get cards from current_block"
     end
