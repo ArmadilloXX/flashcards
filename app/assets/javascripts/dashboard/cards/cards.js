02ac7365ab35b06c0fca7cb7665f91be2ac3bca8
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-10-10 18:30:25 +0300

    Add photo gallery. Fix ajax request to get data

--- app/assets/javascripts/dashboard/cards/cards.js
+++ app/assets/javascripts/dashboard/cards/cards.js
@@ -1,7 +1,19 @@
 $(function() {
   $(".flickr_search").hide();
+
   $('#flickr_button').click(function() {
     $(this).hide();
     $(".flickr_search").fadeIn();
+  });
+
+  $('#search_button').click(function () {
+    $(".panel").remove();
+    var value = $('input[name=search]').val();
+    $.ajax({
+      url: "/flickr_search/search_photos",
+      data: {
+        search: value
+      }
+    });
   });
 });
--- app/controllers/dashboard/flickr_search_controller.rb
+++ app/controllers/dashboard/flickr_search_controller.rb
@@ -1,4 +1,20 @@
 class Dashboard::FlickrSearchController < Dashboard::BaseController
   def search_photos
+    @search_term = params[:search]
+    list = flickr.photos.search text: @search_term, per_page: 10
+    puts list.inspect
+    @photos = url_for_photos(list)
+    puts @photos.class
+  end
+
+  private
+
+  def url_for_photos(list)
+    photos = []
+    list.each do |photo|
+      info = flickr.photos.getInfo(photo_id: photo["id"], secret: photo["secret"])
+      photos << FlickRaw.url(info)
+    end
+    photos
   end
 end
