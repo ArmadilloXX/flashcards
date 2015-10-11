dee3f8af3502d7ae64801fadd09bec1d75f3cb5a
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-10-11 18:54:01 +0300

    Fix default search. Fix loading indicator positioning

--- app/controllers/dashboard/flickr_search_controller.rb
+++ app/controllers/dashboard/flickr_search_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::FlickrSearchController < Dashboard::BaseController
   def search_photos
     @search_term = params[:search]
-    if @search_term
+    if @search_term.blank?
       list = flickr.photos.getRecent per_page: 10
     else
       list = flickr.photos.search text: @search_term, per_page: 10
