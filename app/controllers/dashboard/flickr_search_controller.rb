9cabf2d40ee70f4148ee6c131568e226db1a4fb7
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-10-13 18:53:51 +0300

    Disable WebMock to check stubbing controller methods

--- app/controllers/dashboard/flickr_search_controller.rb
+++ app/controllers/dashboard/flickr_search_controller.rb
@@ -1,14 +1,22 @@
 class Dashboard::FlickrSearchController < Dashboard::BaseController
+  respond_to :js
   
   def search_photos
     @search_term = params[:search]
     if @search_term.blank?
+      Rails.logger.warn("when search is blank")
       list = Flickrie.get_recent_photos per_page: 10
     else
+      Rails.logger.warn("when search provided")
       list = Flickrie.search_photos text: @search_term, per_page: 10
     end
+    respond_with :js
     @photos = url_for_photos(list)
+    Rails.logger.warn '++++++++++++++++++++++++++++++++'
+    Rails.logger.warn @photos
   end
+
+  private
 
   def url_for_photos(list)
     photos = []
--- spec/controllers/dashboard/flickr_search_controller_spec.rb
+++ spec/controllers/dashboard/flickr_search_controller_spec.rb
@@ -1,21 +1,32 @@
 require "rails_helper"
 
 describe Dashboard::FlickrSearchController do
+  let!(:user) { create(:user) }
+  let!(:register) { @controller.send(:auto_login, user) }
 
-  context "when user provides search term" do
-    it "calls the #search with search term"
+  let(:photo_urls) do
+    [
+      "https://farm6.staticflickr.com/5742/21488769794_9fd96e7eb6_q.jpg",
+      "https://farm1.staticflickr.com/601/21923710398_52b0732469_q.jpg",
+      "https://farm1.staticflickr.com/705/21923710458_6fcb52ee05_q.jpg",
+      "https://farm6.staticflickr.com/5727/22085412296_74760fb3f6_q.jpg",
+      "https://farm1.staticflickr.com/688/22085412486_6a861b5e09_q.jpg",
+      "https://farm1.staticflickr.com/618/22085412706_f3a85c67ef_q.jpg",
+      "https://farm6.staticflickr.com/5724/22111607515_568962fa65_q.jpg",
+      "https://farm1.staticflickr.com/585/22111607615_c35cff43db_q.jpg",
+      "https://farm1.staticflickr.com/746/22111607905_cf03bc7571_q.jpg",
+      "https://farm6.staticflickr.com/5760/22111608245_8886649659_q.jpg"
+    ]
   end
 
-  context "when user does not provide search term" do
-
-    it "calls the #getRecent" do
-      get :search_photos
-      # expect(Flickrie).to receive(:get_recent_photos)
-    end
+  before do
+    allow_any_instance_of(Dashboard::FlickrSearchController).to receive(:url_for_photos).with(any_args).and_return(photo_urls)
+    Dashboard::FlickrSearchController.new.send(:url_for_photos, [1,2,3])
   end
 
-  it "responds with js"
-  it "returns an array of photos url"
-  it "returns SOMETHING when no connection" #need to fix gallery template
-
+  it "calls the Flickrie.get_recent_photos when no search params" do
+    xhr :get, :search_photos, {search: '', format: 'js'}
+    
+    expect(assigns(:photos)).to eq(photo_urls)
+  end
 end
