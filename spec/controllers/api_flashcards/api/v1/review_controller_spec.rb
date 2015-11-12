f559575ad4f4ee7c79aa4ad5707d229affdfb531
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-11-12 18:19:53 +0300

    Prefill review_controller spec. Minor changes in shared examples'

--- spec/controllers/api_flashcards/api/v1/review_controller_spec.rb
+++ spec/controllers/api_flashcards/api/v1/review_controller_spec.rb
@@ -6,5 +6,26 @@ module ApiFlashcards
     routes { ApiFlashcards::Engine.routes }
     it_behaves_like "http basic authentification", "get", "index"
     it_behaves_like "http basic authentification", "put", "check"
+
+    context "with correct credentials" do
+      describe "GET#index" do
+        it "responds with 200 status code"
+        it "responds with \'card\'' key in response"
+        it "respond with only one card"
+      end
+
+      describe "PUT#check" do
+        it "responds with 200 status code"
+        context "when user provides correct translation" do
+          it "responds with successful message"
+        end
+        context "when user made a typo" do
+          it "responds with typo message"
+        end
+        context "when user provides incorrect translation" do
+          it "responds with failure message"
+        end
+      end
+    end
   end
 end
--- spec/shared_examples/shared_examples_for_api_controllers.rb
+++ spec/shared_examples/shared_examples_for_api_controllers.rb
@@ -35,13 +35,16 @@ module ApiFlashcards
   end
 
   RSpec.shared_examples "unprocessable entity response" do |card_params|
-    let!(:original_text) { card_params.fetch(:original_text, nil) }
-    let!(:translated_text) { card_params.fetch(:translated_text, nil) }
-    let!(:block_id) { card_params.fetch(:block_id, nil ) }
+    @original_text = card_params.fetch(:original_text, "")
+    @translated_text = card_params.fetch(:translated_text, "")
+    @block_id = card_params.fetch(:block_id, nil)
 
-    describe "original_text=\'#{card_params.fetch(:original_text, nil) }\', "\
-    "translated_text = \'#{card_params.fetch(:translated_text, nil)}\', "\
-    "block_id = #{card_params.fetch(:block_id, nil)}" do
+    describe "original_text=\'#{@original_text}\', "\
+    "translated_text = \'#{@translated_text}\', "\
+    "block_id = #{@block_id}" do
+      let!(:original_text) { card_params.fetch(:original_text, nil) }
+      let!(:translated_text) { card_params.fetch(:translated_text, nil) }
+      let!(:block_id) { card_params.fetch(:block_id, nil ) }
       before do
         post :create,
              request.headers["Authorization"] = encode("test@test.com",
