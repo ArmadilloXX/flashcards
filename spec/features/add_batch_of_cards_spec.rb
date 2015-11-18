c65a4eb1c92819e733032b9305d0308bcb789656
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-11-18 15:41:27 +0300

    Add cards batch adding feature spec

---
+++ spec/features/add_batch_of_cards_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+require "support/helpers/login_helper.rb"
+include LoginHelper
+
+describe "Add batch of cards from provided url" do
+  let!(:user) { create(:user, locale: "ru") }
+  let!(:block) { create(:block, user: user) }
+  before(:each) do
+    login_with("test@test.com", "12345", "Войти")
+
+    # TODO: Add visit path to page
+  end
+
+  describe "user enters the page" do
+    it "contains url field"
+    it "contains original text CSS selector field"
+    it "contains translated text CSS selector field"
+    it "contains block collection field"
+    it "contains Start Process button"
+  end
+
+  describe "user fills the fields and start the adding process" do
+    it "starts the process"
+  end
+
+  describe "process is finished" do
+    it "provides the result of the process to the user"
+  end
+end
