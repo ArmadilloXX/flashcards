34a955a0d5214d6ce9b2a79d7c3a9540a0eefc2c
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-11-19 10:05:07 +0300

    Add test cases for elements on the page

--- spec/features/add_batch_of_cards_spec.rb
+++ spec/features/add_batch_of_cards_spec.rb
@@ -5,18 +5,28 @@ include LoginHelper
 describe "Add batch of cards from provided url" do
   let!(:user) { create(:user, locale: "ru") }
   let!(:block) { create(:block, user: user) }
-  
+
   before(:each) do
     login_with("test@test.com", "12345", "Войти")
     visit new_batch_cards_path
   end
 
   describe "user enters the page" do
-    it "contains url field"
-    it "contains original text CSS selector field"
-    it "contains translated text CSS selector field"
-    it "contains block collection field"
-    it "contains Start Process button"
+    it "contains url field" do
+      expect(page).to have_selector("#batch_url")
+    end
+    it "contains original text CSS selector field" do
+      expect(page).to have_selector("#batch_original_selector")
+    end
+    it "contains translated text CSS selector field" do
+      expect(page).to have_selector("#batch_translated_selector")
+    end
+    it "contains block collection field" do
+      expect(page).to have_selector("#batch_block_id")
+    end
+    it "contains Add cards button" do
+      expect(page).to have_selector("#add_cards_button")
+    end
   end
 
   describe "user fills the fields and start the adding process" do
