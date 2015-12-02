63f688fe3933f7ee48b5de3de3f4e00923bc3c9b
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-02 19:41:40 +0300

    Fix HoundCI codestyle violations

--- app/controllers/dashboard/flickr_search_controller.rb
+++ app/controllers/dashboard/flickr_search_controller.rb
@@ -8,9 +8,9 @@ module Dashboard
                Flickr.photos.get_recent(per_page: 10)
              else
                Rails.cache.
-                 fetch("flickr_search_#{@search_term}", expires_in: 6.hours) do
-                   Flickr.photos.search(text: @search_term, per_page: 10)
-                 end
+               fetch("flickr_search_#{@search_term}", expires_in: 6.hours) do
+                 Flickr.photos.search(text: @search_term, per_page: 10)
+               end
              end
       @photos = list.map { |photo| photo.square!(150).source_url }
     end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,7 +19,9 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :redis_store, "redis://127.0.0.1:6379/0/flashcards_test", { expires_in: 1.day }
+  config.cache_store = :redis_store,
+                       "redis://127.0.0.1:6379/0/flashcards_test",
+                       { expires_in: 1.day }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
