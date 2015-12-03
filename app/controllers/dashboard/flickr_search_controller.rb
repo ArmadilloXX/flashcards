34d7a2705306da008326c6902a96f573ed87bed0
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-03 11:11:56 +0300

    Fix mentor comments

--- app/controllers/dashboard/flickr_search_controller.rb
+++ app/controllers/dashboard/flickr_search_controller.rb
@@ -7,8 +7,8 @@ module Dashboard
       list = if @search_term.blank?
                Flickr.photos.get_recent(per_page: 10)
              else
-               Rails.cache.
-               fetch("flickr_search_#{@search_term}", expires_in: 6.hours) do
+               Rails.cache.fetch("flickr_search_#{@search_term}",
+                                 expires_in: 6.hours) do
                  Flickr.photos.search(text: @search_term, per_page: 10)
                end
              end
