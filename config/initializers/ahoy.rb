0db7df673482d7119e839d390fb9fea7c992a8c5
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-14 18:35:22 +0300

    Test Firehose client to send visit data to stream

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,4 +1,6 @@
+require "aws-sdk"
 class Ahoy::Store < Ahoy::Stores::LogStore
+
   def visit
     user_id = user.id if user
     @visit ||= {
@@ -10,16 +12,25 @@ class Ahoy::Store < Ahoy::Stores::LogStore
 
   protected
 
-  # def log_visit(data)
-  #   visit_logger.info data.to_json
-  # end
+  def firehose
+    @firehose ||= Aws::Firehose::Client.new(region: 'us-west-2')
+  end
+
+  def log_visit(data)
+    firehose.put_record({
+      delivery_stream_name: visit_stream,
+      record: {
+        data: data.to_json
+      }
+    }) 
+  end
 
   # def log_event(data)
   #   event_logger.info data.to_json
   # end
 
   def visit_stream
-
+    'test-stream'
   end
 
   def event_stream
