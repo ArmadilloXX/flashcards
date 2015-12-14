76f731e3cd0f42806bd1105f32b749a984432700
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-14 17:55:52 +0300

    Switch to Ahoy::Stores::LogStore instead of BaseStoree

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,33 +1,4 @@
-class Ahoy::Store < Ahoy::Stores::BaseStore
-  def track_visit(options, &block)
-    data = {
-      id: ahoy.visit_id,
-      visitor_id: ahoy.visitor_id
-    }.merge(visit_properties.to_hash)
-    data[:user_id] = user.id if user
-    data[:started_at] = options[:started_at]
-
-    yield(data) if block_given?
-
-    log_visit(data)
-  end
-
-  def track_event(name, properties, options, &block)
-    data = {
-      id: options[:id],
-      name: name,
-      properties: properties,
-      visit_id: ahoy.visit_id,
-      visitor_id: ahoy.visitor_id
-    }
-    data[:user_id] = user.id if user
-    data[:time] = options[:time]
-
-    yield(data) if block_given?
-
-    log_event(data)
-  end
-
+class Ahoy::Store < Ahoy::Stores::LogStore
   def visit
     user_id = user.id if user
     @visit ||= {
@@ -39,20 +10,19 @@ class Ahoy::Store < Ahoy::Stores::BaseStore
 
   protected
 
-  def log_visit(data)
-    visit_logger.info data.to_json
+  # def log_visit(data)
+  #   visit_logger.info data.to_json
+  # end
+
+  # def log_event(data)
+  #   event_logger.info data.to_json
+  # end
+
+  def visit_stream
+
   end
 
-  def log_event(data)
-    event_logger.info data.to_json
-  end
+  def event_stream
 
-  # TODO disable header
-  def visit_logger
-    @visit_logger ||= ActiveSupport::Logger.new(Rails.root.join("log/visits.log"))
-  end
-
-  def event_logger
-    @event_logger ||= ActiveSupport::Logger.new(Rails.root.join("log/events.log"))
   end
 end
