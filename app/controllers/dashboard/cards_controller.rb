fa97b8a5eb3db69644faa40b799864521a3765b2
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-14 13:37:40 +0300

    Configure Ahoy to track

--- app/controllers/dashboard/cards_controller.rb
+++ app/controllers/dashboard/cards_controller.rb
@@ -3,6 +3,9 @@ class Dashboard::CardsController < Dashboard::BaseController
 
   def index
     @cards = current_user.cards.all.order("review_date")
+    Rails.logger.debug "==========================="
+    Rails.logger.debug current_visit
+    Rails.logger.debug "==========================="
   end
 
   def new
