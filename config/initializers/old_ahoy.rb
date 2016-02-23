a13b2030cb955506e92f17c93de471b67bcd2a79
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-02-23 17:19:04 +0300

    Comment old config files

--- config/initializers/old_ahoy.rb
+++ config/initializers/old_ahoy.rb
@@ -1,24 +1,21 @@
-# require "aws-sdk"
-# require "elasticsearch"
 # require "active_support/core_ext/date_time/conversions"
 # class Ahoy::Store < Ahoy::Stores::LogStore
+#   attr_reader :firehose, :elastic
 
-#   def firehose_client
-#     @firehose_client ||= Aws::Firehose::Client.new
+#   def initialize(options)
+#     super
+#     @firehose = FirehoseLogger.new
+#     @elastic = ElasticLogger.new
 #   end
 
-#   def elastic_client
-#     @elastic_client ||= Elastic.new
-#   end
-
-#   def visit
-#     user_id = user.id if user
-#     @visit ||= {
-#       id: ahoy.visit_id,
-#       visitor_id: ahoy.visitor_id,
-#       user_id: user_id
-#     }.merge(visit_properties.to_hash)
-#   end
+#   # def visit
+#   #   user_id = user.id if user
+#   #   @visit ||= {
+#   #     id: ahoy.visit_id,
+#   #     visitor_id: ahoy.visitor_id,
+#   #     user_id: user_id
+#   #   }.merge(visit_properties.to_hash)
+#   # end
 
 #   protected
 
@@ -26,7 +23,9 @@
 #     # db_timestamp = convert_to_db_timestamp(data[:started_at])
 #     # data[:started_at] = db_timestamp
 #     # send_to_stream(visit_stream, data)
-#     elastic_client.log_visit(data)
+#     # elastic_client.log_visit(data)
+#     LogVisitToFirehoseJob.perform_later(data)
+#     LogVisitToElasticJob.perform_later(data)
 #   end
 
 #   def log_event(data)
@@ -35,7 +34,9 @@
 #     # log_event_properties(data) unless data[:properties].empty?
 #     # my_data = data.reject { |k| k == :properties }
 #     # send_to_stream(event_stream, my_data)
-#     elastic_client.log_event(data)
+#     # elastic_client.log_event(data)
+#     LogEventToFirehoseJob.perform_later(data)
+#     LogEventToElasticJob.perform_later(data)
 #   end
 
 #   # def log_event_properties(data)
@@ -44,15 +45,6 @@
 #   #     property[:id] = "#{data[:id]}-#{event_properties.index(property)}"
 #   #     send_to_stream(event_properties_stream, property)
 #   #   end
-#   # end
-
-#   # def send_to_stream(stream_name, data)
-#   #   firehose_client.put_record({
-#   #     delivery_stream_name: stream_name,
-#   #     record: {
-#   #       data: data.to_json
-#   #     }
-#   #   })
 #   # end
 
 #   # def prepare_event_properties(event_id, properties)
@@ -69,14 +61,6 @@
 
 #   # def convert_to_db_timestamp(datetime)
 #   #   datetime.utc.to_formatted_s(:db)
-#   # end
-
-#   # def visit_stream
-#   #   "visits"
-#   # end
-
-#   # def event_stream
-#   #   "events"
 #   # end
 
 #   # def event_properties_stream
